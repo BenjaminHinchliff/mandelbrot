62088ce7075acc0c4d0f9cad341dc60d0e825ff1
SuniTheFish <benjamin.hinchliff@gmail.com>
2019-07-27 16:54:39 -0700

    added better argument error handling

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,14 @@ fn main() {
     }
 
     let mut config = Config::new(
-        args[1].parse().unwrap(),
-        args[2].parse().unwrap(),
-        args[3].parse().unwrap(),
+        args[1].parse()
+            .expect("Unable to parse the width"),
+        args[2].parse()
+            .expect("Unable to parse height"),
+        args[3].parse()
+            .expect("Unable to parse iterations"),
     );
     run(&mut config);
     
-
     image::save_buffer(&Path::new("image.png"), &config.buffer, config.width, config.height, image::RGB(8)).unwrap();
 }
